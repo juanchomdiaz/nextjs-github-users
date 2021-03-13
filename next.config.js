7a8348c7de4dfbeb7b0c62a244245e88cdaf048f
Juan Manuel <juanmanueldiaz85@gmail.com>
2021-03-13 18:45:59 -0300

    Updated next config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicRuntimeConfig: {
-      pokeapiURL: 'https://api.github.com/',
+      pokeapiURL: 'https://api.github.com/', /* I can read variables from process.env here. Useful in multiples environments. */
       displayLimit: 12,
     },
 };
