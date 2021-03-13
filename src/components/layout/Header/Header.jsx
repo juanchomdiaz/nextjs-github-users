71535bea23eb93a3e91d28feb3cd9d3e32d3530b
Juan Manuel <juanmanueldiaz85@gmail.com>
2021-03-13 19:10:34 -0300

    Fixed styles for Header component

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,14 +1,14 @@
 import { Navbar } from "react-bootstrap";
 
-import "./Header.module.css";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <Navbar bg="light" variant="light" className="justify-content-between">
       <a href="/">
-        <Navbar.Brand>
-          <img alt="Github Users Logo" src="/images/svg/logo.svg" width="40" height="40" className="logo" />{" "}
-          <div className="brand-name">Github Users</div>
+        <Navbar.Brand className={styles.navbarBrand}>
+          <img alt="Github Users Logo" src="/images/svg/logo.svg" width="40" height="40" className={styles.navbarLogo} />
+          <div className={styles.brandName}>Github Users</div>
         </Navbar.Brand>
       </a>
     </Navbar>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,6 +11,11 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
 
         <link
+          href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600&display=swap"
+          rel="stylesheet"
+        />
+        
+        <link
           rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
           integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
