8f0bff69e28f3568efd6cc0ccbb9c59babe5b6a1
Juan Manuel <juanmanueldiaz85@gmail.com>
2021-03-13 19:16:31 -0300

    Moved global head to _document file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,27 +1,10 @@
 import "../styles/globals.css";
 
-import Head from "next/head";
-
 import AppLayout from "@components/layout/AppLayout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-
-        <link
-          href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600&display=swap"
-          rel="stylesheet"
-        />
-        
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-          crossOrigin="anonymous"
-        />
-      </Head>
       <AppLayout>
         <Component {...pageProps} />
       </AppLayout>
---
+++ src/pages/_document.js
@@ -0,0 +1,37 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600&display=swap"
+            rel="stylesheet"
+          />
+
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+            crossOrigin="anonymous"
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
