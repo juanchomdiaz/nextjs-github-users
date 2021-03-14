13b1148ecc15ebcd8bccb3164b0c5c2236242ca7
Juan Manuel <juanmanueldiaz85@gmail.com>
2021-03-14 12:52:58 -0300

    Changed index page in order to load users and show the list with ssr

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,33 @@
+import Head from 'next/head';
 
-export default function Home() {
+import UsersState from '@context/users/UsersState';
+import UsersListWrapper from '@components/views/UsersListWrapper';
+
+import getConfig from 'next/config';
+
+const {
+  publicRuntimeConfig: { firstPageUrl, userPerPage },
+} = getConfig();
+
+export default function Main({ users }) {
   return (
-    <></>
-  )
+    <>
+      <Head>
+        <title>Github's Users Browser</title>
+      </Head>
+      <UsersState users={users}>
+        <UsersListWrapper />
+      </UsersState>
+    </>
+  );
 }
+
+export const getServerSideProps = async () => {
+  const users = await githubapiService.getUsers(firstPageUrl, userPerPage);
+
+  return {
+    props: {
+      users,
+    },
+  };
+};
