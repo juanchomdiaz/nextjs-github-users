82bf7e336d068c51a2d3ccc20bca55919f7619bf
Juan Manuel <juanmanueldiaz85@gmail.com>
2021-03-14 12:13:19 -0300

    Added component folders and base files for users list page

---
+++ src/components/views/UsersListWrapper/UsersListWrapper.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+
+import { Row, Col } from "react-bootstrap";
+
+import UsersList from "@components/views/UsersList";
+import UsersListPager from "@components/view/users/UsersListPager";
+
+const UsersListWrapper = () => {
+  return (
+    <Fragment>
+      <Row>
+        <Col>
+          <UsersList />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <UsersListPager />
+        </Col>
+      </Row>
+    </Fragment>
+  );
+};
+
+export default UsersListWrapper;
