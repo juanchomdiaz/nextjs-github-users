1d1757b4162fe534b8884e6a7005458b9d43dfad
Juan Manuel <juanmanueldiaz85@gmail.com>
2021-03-15 00:46:38 -0300

    Added alias path to jest config for module resolving

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-    setupFiles: ['<rootDir>/src/tests/setupTest.js'],
-    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-    moduleNameMapper: {
-        "\\.(scss|sass|css)$": "identity-obj-proxy"
-    }
-}
+  setupFiles: ['<rootDir>/src/tests/setupTest.js'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  roots: ['src'],
+  moduleDirectories: ["node_modules", "src"],
+  moduleNameMapper: {
+    '\\.(scss|sass|css)$': 'identity-obj-proxy',
+    "^@components(.*)$": "<rootDir>/src/components$1",
+    "^@context(.*)$": "<rootDir>/src/context$1",
+    "^@hooks(.*)$": "<rootDir>/src/hooks$1",
+    "^@helpers(.*)$": "<rootDir>/src/helpers$1",
+  },
+};
---
+++ src/components/views/UserCard/UserCard.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import UserCard from "./UserCard";
+import UsersContext from "@context/users/UsersContext";
+import { mount } from "enzyme";
+
+
+function renderUserCard(value, userVal) {
+  return mount(
+      <UsersContext.Provider value={{ isReady: value }}>
+        <UserCard user={userVal} />
+      </UsersContext.Provider>
+  );
+}
+
+const fakeUser = {
+    login: "mojombo",
+    id: 1,
+    avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+    html_url: "https://github.com/mojombo",
+    public_repos: 62,
+    public_gists: 62,
+    followers: 22389,
+    following: 11,
+};
+
+describe("UserCard", () => {
+    it(">>> Should render the card with the user picture", () => {
+        const wrapper = renderUserCard(true, fakeUser);
+        setTimeout(() => {
+          expect(wrapper.find(".card-img").exists()).toBeTruthy();
+        }, 3000)
+      });
+
+    it(">>> Should render the card skeleton", () => {
+        const wrapper = renderUserCard(false, fakeUser);
+        setTimeout(() => {
+          expect(wrapper.find(".react-loading-skeleton").exists()).toBeTruthy();
+        }, 2000)
+      });
+});
+
+
